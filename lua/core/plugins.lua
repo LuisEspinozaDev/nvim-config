05268ccbd6396303b603296feccfcb3cc2883d3e
Luis Espinoza <lespinozave@gmail.com>
2023-06-01 22:00:58 -0500

    feat: update theme

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -13,8 +13,7 @@ local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
---  use 'ellisonleao/gruvbox.nvim'
-  use 'rebelot/kanagawa.nvim'
+  use { "catppuccin/nvim", as = "catppuccin" }
   use 'nvim-tree/nvim-tree.lua'
   use 'nvim-tree/nvim-web-devicons'
   use 'nvim-lualine/lualine.nvim'
